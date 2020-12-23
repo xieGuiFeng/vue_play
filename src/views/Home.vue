1fc1eff48d499c51e8871bad1627b7622bda3e42
谢桂锋 <13580190510@163.com>
2020-12-23 16:45:57 +0800

    V1.0.0版本

---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+	<router-view></router-view>  
+	<ul class="footer">
+		<li class="icons">
+			<router-link :to="{name: 'list'}">新闻列表</router-link>
+		</li>
+		<li class="icons">
+			<router-link :to="{name: 'user'}">个人中心</router-link>
+		</li>
+	</ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style scoped lang="scss">
+	li{
+		list-style: none;
+	}
+	.footer{
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 60px;
+		line-height: 60px;
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: space-around;
+	}
+	.icons{
+		font-size: 16px;
+		flex: 1;
+		text-align: center;
+		border-top: 1px solid #42B983;
+		a{
+			color: #42B983;
+			display: block;
+			&.active{
+				color: #fff;
+				background-color: #42B983;
+			}
+		}
+	}
+	
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<p>Usser</p>
+		<button @click="add">添加</button>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: "User",
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			add () {
+				this.$router.push('/add')
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
